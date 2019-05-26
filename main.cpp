63793008d9da0b88a5d90d8afe35b73c7df77959
Max Base <MaxBaseCode@Gmail.Com>
2019-05-26 00:00:34 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+double root(double n){
+	double low = 0;
+	double hight = n;
+	double middle;
+	double middlePow;
+	for(int i = 0 ; i < 40 ; i++){
+		middle = (low+hight)/2;
+		middlePow = middle*middle;
+		if(middlePow == n)
+			return middle;
+		else if(middlePow > n)
+			hight = middle;
+		else
+			low = middle;
+	}
+	return middle;
+}
+
+int main() {
+	cout << root(4) << "\n";
+	cout << root(5) << "\n";
+	cout << root(15) << "\n";
+	cout << root(16) << "\n";
+	return 0;
+}
